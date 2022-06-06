9a5ec30e6a5749a4a31db1edb08defdc0cd0372b
Callum McDowell <cmcd407@aucklanduni.ac.nz>
2022-06-07 00:50:28 +1200

    Validated that NoC streaming input also works

--- IRPs/SpectrogramASP/quartus/testbenches/DFT_Testbench.vhd
+++ IRPs/SpectrogramASP/quartus/testbenches/DFT_Testbench.vhd
@@ -144,7 +144,7 @@ begin
         noc_recv.data <= (others => '0');
 
 
-        -- enable via NoC to validate NoC support
+        -- enable via NoC
         noc_recv.data(31) <= '1';
         noc_recv.data(27 downto 24) <= x"2"; -- NOC_SET_ENABLE
 
@@ -192,7 +192,76 @@ begin
         file_close(file_test_signal);
         file_close(file_log);
 
-        report "LOGGING COMPLETE";
+        report "x-DIRECT LOGGING COMPLETE";
+
+
+        -- -- With NoC Feed
+
+
+        -- set input mode as NoC via NoC
+        noc_recv.data(31) <= '1';
+        noc_recv.data(27 downto 24) <= x"6"; -- NOC_SET_INPUT_MODE
+        noc_recv.data(15 downto 0) <= x"0001"; -- NOC_INPUT_MODE_NOC
+        wait for CLK_PERIOD;
+        noc_recv.data <= (others => '0');
+
+        -- set run mode as auto via NoC
+        noc_recv.data(31) <= '1';
+        noc_recv.data(27 downto 24) <= x"5"; -- NOC_SET_RUN_MODE
+        noc_recv.data(15 downto 0) <= x"0001"; -- NOC_RUN_MODE_AUTO
+        wait for CLK_PERIOD;
+        noc_recv.data <= (others => '0');
+
+
+        -- Test with generated data
+        new_window <= '1';
+        wait until rising_edge(clk);
+        wait for CLK_PERIOD/2;
+        new_window <= '0';
+
+        file_open(file_test_signal, "int16_signal.txt", read_mode);
+
+        for x_idx in 0 to WINDOW_WIDTH-1 loop
+            wait for CLK_PERIOD/5;
+
+            readline(file_test_signal, test_signal_line);
+            read(test_signal_line, test_signal_int);
+            
+            test_signal_sample := to_signed(test_signal_int, signal_word'length);
+            
+            noc_recv.data(31) <= '1';
+            noc_recv.data(27 downto 24) <= x"3"; -- NOC_NEW_SAMPLE
+            noc_recv.data(15 downto 0) <= std_logic_vector(test_signal_sample);
+
+            wait until rising_edge(clk);
+
+            wait for CLK_PERIOD/5;
+            noc_recv.data <= (others => '0'); -- simulate gap between packets in NoC
+            
+            wait until rising_edge(clk);
+        end loop;
+
+
+        -- enable via NoC
+        noc_recv.data(31) <= '1';
+        noc_recv.data(27 downto 24) <= x"2"; -- NOC_SET_ENABLE
+
+        wait for CLK_PERIOD;
+        rst <= '1';
+        wait for CLK_PERIOD;
+        rst <= '0';
+
+
+        file_open(file_log, "test_log_noc.txt", write_mode);
+        for k_idx in 0 to K_LENGTH-1 loop
+            write(log_line, to_integer(magnitudes(k_idx)));
+            writeline(file_log, log_line);
+        end loop;
+
+        file_close(file_test_signal);
+        file_close(file_log);
+
+        report "x-NoC LOGGING COMPLETE";
         wait;
 
     end process test;
