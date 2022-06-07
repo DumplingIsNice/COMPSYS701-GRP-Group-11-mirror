20752f50d430a2b462aea1fe8c2c148c2fc25b2b
Callum McDowell <cmcd407@aucklanduni.ac.nz>
2022-06-07 14:42:05 +1200

    Reduce DFT resolution (add k-step of 4, 1/4 of max resolution)

--- GRP/proof-of-concept/quartus/rtl/DataProcessingASP/DFTTypes.vhd
+++ GRP/proof-of-concept/quartus/rtl/DataProcessingASP/DFTTypes.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_1164.all;
 use ieee.math_real.all;
 
 package DFTTypes is
+    constant    SAMPLE_FREQ                 : positive          := 24000;
   
     constant    SINUSOID_WORD_LENGTH        : natural           := 16;
     subtype     signed_fxp_sinusoid is      signed(SINUSOID_WORD_LENGTH-1 downto 0);
@@ -14,7 +15,8 @@ package DFTTypes is
     type        signal_window is            array (WINDOW_WIDTH-1 downto 0) of signal_word;
     subtype     signed_correlation_sum is   signed(WINDOW_WORD_LENGTH + natural(ceil(log2(real(WINDOW_WIDTH))))-1 downto 0);
 
-    constant    K_LENGTH                    : natural           := WINDOW_WIDTH/2;
+    constant    K_STEP                      : natural           := 4; -- 1 for max resolution
+    constant    K_LENGTH                    : natural           := (WINDOW_WIDTH/2)/K_STEP;
     -- type        sinusoid_k_value_array is   array (2*K_LENGTH-1 downto 0) of signed_fxp_sinusoid;
     --
     type        magnitudes_array is         array (K_LENGTH-1 downto 0) of signal_word; 
